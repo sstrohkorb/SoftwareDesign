3b8bde1d823ce90be90d6301c9e3d940821f8cd5
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-02-14 11:11:39 -0500

    Up through the ORF finding

--- hw3/amino_acids.py
+++ hw3/amino_acids.py
@@ -2,25 +2,24 @@ aa = ['F','L','I','M','V','S','P','T','A','Y',
       '|','H','Q','N','K','D','E','C','W','R',
       'G']
 
-codons = [['TTT', 'TTC'],
-          ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'],
-          ['ATT', 'ATC', 'ATA'],
-          ['ATG'],
-          ['GTT', 'GTC', 'GTA', 'GTG'],
-          ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],
-          ['CCT', 'CCC', 'CCA', 'CCG'],
-          ['ACT', 'ACC', 'ACA', 'ACG'],
-          ['GCT', 'GCC', 'GCA', 'GCG'],
-          ['TAT', 'TAC'],
-          ['TAA', 'TAG', 'TGA'],
-          ['CAT', 'CAC'],
-          ['CAA', 'CAG'],
-          ['AAT', 'AAC'],
-          ['AAA', 'AAG'],
-          ['GAT', 'GAC'],
-          ['GAA', 'GAG'],
-          ['TGT', 'TGC'],
-          ['TGG'],
-          ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
-          ['GGT', 'GGC', 'GGA', 'GGG']]
-              
+codons = [['TTT', 'TTC'],                                   # F
+          ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'],       # L
+          ['ATT', 'ATC', 'ATA'],                            # I
+          ['ATG'],                                          # M
+          ['GTT', 'GTC', 'GTA', 'GTG'],                     # V
+          ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],       # S
+          ['CCT', 'CCC', 'CCA', 'CCG'],                     # P
+          ['ACT', 'ACC', 'ACA', 'ACG'],                     # T
+          ['GCT', 'GCC', 'GCA', 'GCG'],                     # A
+          ['TAT', 'TAC'],                                   # Y
+          ['TAA', 'TAG', 'TGA'],                            # |
+          ['CAT', 'CAC'],                                   # H
+          ['CAA', 'CAG'],                                   # Q
+          ['AAT', 'AAC'],                                   # N
+          ['AAA', 'AAG'],                                   # K
+          ['GAT', 'GAC'],                                   # D
+          ['GAA', 'GAG'],                                   # E
+          ['TGT', 'TGC'],                                   # C
+          ['TGG'],                                          # W 
+          ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],       # R
+          ['GGT', 'GGC', 'GGA', 'GGG']]                     # G
--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Feb  2 11:24:42 2014
 
-@author: YOUR NAME HERE
+@author: Sarah Strohkorb
 """
 
 # you may find it useful to import these variables (although you are not required to use them)
@@ -15,6 +15,17 @@ def collapse(L):
         output = output + s
     return output
 
+def two_lists_contain_same_elements(list1, list2):
+    if len(list1) != len(list2):
+        return False
+    else: 
+        for list_item in list1:
+            if list_item in list2:
+                continue
+            else:
+                return False
+        return True
+
 
 def coding_strand_to_AA(dna):
     """ Computes the Protein encoded by a sequence of DNA.  This function
@@ -25,13 +36,50 @@ def coding_strand_to_AA(dna):
         returns: a string containing the sequence of amino acids encoded by the
                  the input DNA fragment
     """
-    
-    # YOUR IMPLEMENTATION HERE
+    amino_acid_output = ""
+    for i in xrange(0,len(dna),3):
+        my_codon = dna[i:i+3]
+        for j in range(len(codons)):
+            if my_codon in codons[j]:
+                amino_acid_output += aa[j]
+                break
+    return amino_acid_output
+
 
 def coding_strand_to_AA_unit_tests():
     """ Unit tests for the coding_strand_to_AA function """
-        
-    # YOUR IMPLEMENTATION HERE
+
+    # DNA input strands 
+    dna_input1 = "ACTGCCCC"
+    dna_input2 = "AGCTGAGGGTGTTTTGGA"
+    dna_input3 = "CAGGCTTGCGGCTTCTTAA"
+
+    # Expected output amino acid strands
+    e_output1 = "TA"
+    e_output2 = "S|GCFG"
+    e_output3 = "QACGFL"
+
+    # Actual output amino acid strands 
+    a_output1 = coding_strand_to_AA(dna_input1)
+    a_output2 = coding_strand_to_AA(dna_input2)
+    a_output3 = coding_strand_to_AA(dna_input3)
+
+    test1_result = (e_output1 == a_output1)
+    test2_result = (e_output2 == a_output2)
+    test3_result = (e_output3 == a_output3)
+
+    if not test1_result:
+        print "Test 1 FAILED: " + str(a_output1) + " != " + str(e_output1)
+    if not test2_result:
+        print "Test 2 FAILED: " + str(a_output2) + " != " + str(e_output2)
+    if not test3_result:
+        print "Test 3 FAILED: " + str(a_output3) + " != " + str(e_output3)
+
+    if test1_result and test2_result and test3_result:
+        return True
+    else:
+        return False
+    
 
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
@@ -40,13 +88,55 @@ def get_reverse_complement(dna):
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
     """
-    
-    # YOUR IMPLEMENTATION HERE
+    reverse_complement_output = ""
+    for i in range(len(dna)):
+        base = dna[len(dna)-(i+1)]
+        complementary_base = ""
+        if base == 'T':
+            complementary_base = 'A'
+        elif base == 'A':
+            complementary_base = 'T'
+        elif base == 'G':
+            complementary_base = 'C'
+        elif base == 'C':
+            complementary_base = 'G'
+        reverse_complement_output += complementary_base
+    return reverse_complement_output
     
 def get_reverse_complement_unit_tests():
     """ Unit tests for the get_complement function """
-        
-    # YOUR IMPLEMENTATION HERE    
+
+    # DNA input strands 
+    dna_input1 = "ACTGCCCC"
+    dna_input2 = "AGCTGAGGGTGTTTTGGA"
+    dna_input3 = "CAGGCTTGCGGCTTCTTAA"
+
+    # Expected output DNA strands
+    e_output1 = "GGGGCAGT"
+    e_output2 = "TCCAAAACACCCTCAGCT"
+    e_output3 = "TTAAGAAGCCGCAAGCCTG"
+
+    # Actual output DNA strands
+    a_output1 = get_reverse_complement(dna_input1)
+    a_output2 = get_reverse_complement(dna_input2)
+    a_output3 = get_reverse_complement(dna_input3)
+
+    test1_result = (e_output1 == a_output1)
+    test2_result = (e_output2 == a_output2)
+    test3_result = (e_output3 == a_output3)
+
+    if not test1_result:
+        print "Test 1 FAILED: " + str(a_output1) + " != " + str(e_output1)
+    if not test2_result:
+        print "Test 2 FAILED: " + str(a_output2) + " != " + str(e_output2)
+    if not test3_result:
+        print "Test 3 FAILED: " + str(a_output3) + " != " + str(e_output3)
+
+    if test1_result and test2_result and test3_result:
+        return True
+    else:
+        return False
+
 
 def rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start codon and returns
@@ -56,13 +146,52 @@ def rest_of_ORF(dna):
         dna: a DNA sequence
         returns: the open reading frame represented as a string
     """
-    
-    # YOUR IMPLEMENTATION HERE
+    ORF_output = ""
+    for i in xrange(0,len(dna),3):
+        my_codon = dna[i:i+3]
+        if my_codon == 'TAA' or my_codon == 'TAG' or my_codon == 'TGA':
+            return ORF_output
+        else:
+            ORF_output += my_codon
+    return ORF_output
 
 def rest_of_ORF_unit_tests():
     """ Unit tests for the rest_of_ORF function """
-        
-    # YOUR IMPLEMENTATION HERE
+
+    # DNA input strands 
+    dna_input1 = "ATGTTTAAGCCGTAAATGAAACCGGGC"
+    dna_input2 = "ATGTAG"
+    dna_input3 = "ATGCCGATAGCCTGACCGATAAAATTG"
+    dna_input4 = "ATGC"
+
+    # Expected output DNA strands
+    e_output1 = "ATGTTTAAGCCG"
+    e_output2 = "ATG"
+    e_output3 = "ATGCCGATAGCC"
+    e_output4 = "ATGC"
+
+    # Actual output DNA strands
+    a_output1 = rest_of_ORF(dna_input1)
+    a_output2 = rest_of_ORF(dna_input2)
+    a_output3 = rest_of_ORF(dna_input3)
+    a_output4 = rest_of_ORF(dna_input4)
+
+    test1_result = (e_output1 == a_output1)
+    test2_result = (e_output2 == a_output2)
+    test3_result = (e_output3 == a_output3)
+
+    if not test1_result:
+        print "Test 1 FAILED: " + str(a_output1) + " != " + str(e_output1)
+    if not test2_result:
+        print "Test 2 FAILED: " + str(a_output2) + " != " + str(e_output2)
+    if not test3_result:
+        print "Test 3 FAILED: " + str(a_output3) + " != " + str(e_output3)
+
+    if test1_result and test2_result and test3_result:
+        return True
+    else:
+        return False
+
         
 def find_all_ORFs_oneframe(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence and returns
@@ -74,13 +203,52 @@ def find_all_ORFs_oneframe(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
-     
-    # YOUR IMPLEMENTATION HERE        
+    all_ORFs_output = []
+    dna_length = len(dna)
+    current_dna_location = 0
+    while(current_dna_location < dna_length):
+        my_codon = dna[current_dna_location : current_dna_location + 3]
+        if my_codon == 'ATG':
+            discovered_ORF = rest_of_ORF(dna[current_dna_location : dna_length])
+            all_ORFs_output.append(discovered_ORF)
+            current_dna_location += len(discovered_ORF)
+        else:
+            current_dna_location += 3
+    return all_ORFs_output
      
 def find_all_ORFs_oneframe_unit_tests():
     """ Unit tests for the find_all_ORFs_oneframe function """
 
-    # YOUR IMPLEMENTATION HERE
+    # DNA input strands 
+    dna_input1 = "ATGCATGAATGTAGATAGATGTGCCC"
+    dna_input2 = "ATGCCCGGGTATCCGGAAATAG"
+    dna_input3 = "CCGTTTATGCCGTAGTTAGACATGCCCGAGTAAGCGATGTTTATAGGGC"
+
+    # Expected output list of ORFs
+    e_output1 = ['ATGCATGAATGTAGA', 'ATGTGCCC']
+    e_output2 = ['ATGCCCGGGTATCCGGAAATAG']
+    e_output3 = ['ATGCCG', 'ATGCCCGAG', 'ATGTTTATAGGGC']
+
+    # Actual output list of ORFs
+    a_output1 = find_all_ORFs_oneframe(dna_input1)
+    a_output2 = find_all_ORFs_oneframe(dna_input2)
+    a_output3 = find_all_ORFs_oneframe(dna_input3)
+
+    test1_result = two_lists_contain_same_elements(e_output1, a_output1)
+    test2_result = two_lists_contain_same_elements(e_output2, a_output2)
+    test3_result = two_lists_contain_same_elements(e_output3, a_output3)
+
+    if not test1_result:
+        print "Test 1 FAILED: " + str(a_output1) + " != " + str(e_output1)
+    if not test2_result:
+        print "Test 2 FAILED: " + str(a_output2) + " != " + str(e_output2)
+    if not test3_result:
+        print "Test 3 FAILED: " + str(a_output3) + " != " + str(e_output3)
+
+    if test1_result and test2_result and test3_result:
+        return True
+    else:
+        return False
 
 def find_all_ORFs(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence in all 3
@@ -91,13 +259,54 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
-     
-    # YOUR IMPLEMENTATION HERE
+    dna_frame_0 = dna
+    dna_frame_1 = dna[1:]
+    dna_frame_2 = dna[2:]
+
+    ORFs_frame_0 = find_all_ORFs_oneframe(dna_frame_0)
+    ORFs_frame_1 = find_all_ORFs_oneframe(dna_frame_1)
+    ORFs_frame_2 = find_all_ORFs_oneframe(dna_frame_2)
+
+    return ORFs_frame_0 + ORFs_frame_1 + ORFs_frame_2
+
 
 def find_all_ORFs_unit_tests():
     """ Unit tests for the find_all_ORFs function """
-        
-    # YOUR IMPLEMENTATION HERE
+
+    # DNA input strands 
+    # tests offset by 2 and 0 ORFs, multiple ORFs
+    dna_input1 = "ATGCATGAATGTAGATAGATGTGCCC"
+    # tests offset by two and only one ORF
+    dna_input2 = "GCATGCCCGGGTATCCGGAAATAG"
+    # nested reading frame, that won't be included
+    dna_input3 = "GATGTTTATGGGCTAGTAG"
+
+    # Expected output list of ORFs
+    e_output1 = ['ATGCATGAATGTAGA', 'ATGTGCCC', 'ATGAATGTAGATAGATGTGCCC', 'ATG']
+    e_output2 = ['ATGCCCGGGTATCCGGAAATAG']
+    e_output3 = ['ATGTTTATGGGC']
+
+    # Actual output list of ORFs
+    a_output1 = find_all_ORFs(dna_input1)
+    a_output2 = find_all_ORFs(dna_input2)
+    a_output3 = find_all_ORFs(dna_input3)
+
+    test1_result = two_lists_contain_same_elements(e_output1, a_output1)
+    test2_result = two_lists_contain_same_elements(e_output2, a_output2)
+    test3_result = two_lists_contain_same_elements(e_output3, a_output3)
+
+    if not test1_result:
+        print "Test 1 FAILED: " + str(a_output1) + " != " + str(e_output1)
+    if not test2_result:
+        print "Test 2 FAILED: " + str(a_output2) + " != " + str(e_output2)
+    if not test3_result:
+        print "Test 3 FAILED: " + str(a_output3) + " != " + str(e_output3)
+
+    if test1_result and test2_result and test3_result:
+        return True
+    else:
+        return False
+
 
 def find_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
@@ -147,3 +356,13 @@ def gene_finder(dna, threshold):
     """
 
     # YOUR IMPLEMENTATION HERE
+
+if __name__ == "__main__":
+    print str(coding_strand_to_AA_unit_tests()) + " - DNA to AA strand conversion"
+    print str(get_reverse_complement_unit_tests()) + " - reverse complement of DNA strand"
+    print str(rest_of_ORF_unit_tests()) + " - get ORF that starts with a start codon"
+    print str(find_all_ORFs_oneframe_unit_tests()) + ' - find all ORFs one frame'
+    print str(find_all_ORFs_unit_tests()) + ' - find all ORFs all frames'
+
+
+
